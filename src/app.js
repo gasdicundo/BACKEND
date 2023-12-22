150820f887085c180a9777b9764327d35f5a338c
gasdicundo <dicundogaston@gmail.com>
2023-12-22 10:45:01 -0300

    Desafio

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ const io = socketIO(server);
 
 
 app.engine('handlebars', exphbs({
-  layoutsDir: path.join(__dirname, 'views', 'layouts'),
-  defaultLayout: 'main',
-  extname: '.handlebars',
-}));
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+	layoutsDir: path.join(__dirname, 'views', 'layouts'),
+	defaultLayout: 'main',
+	extname: '.handlebars',
+  }));
+  app.set('view engine', 'handlebars');
+  app.set('views', path.join(__dirname, 'views'));
 
-// Rutas
+
 const productos =[
     {
         "id": 1,
